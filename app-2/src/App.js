a2a2907e775e99db04fc96c1f62534f0b67bbaac
Aster Brynn <aster.gibby@gmail.com>
2019-04-02 18:18:02 -0600

    init commit

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      arr: ["spaghoot", "pizza", "croissants", "sausage", "milk"]
+    }
+  }
   render() {
+    let list = this.state.arr.map((val, i) => <h3 key={i}>{val}</h3>)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {list}
       </div>
     );
   }
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      arr: ["spaghoot", "pizza", "croissants", "sausage", "milk"]
+    }
+  }
+
+  updateFilter = e => {
+    console.log(e)
+    this.setState({
+      userInput: e
+    })
+  }
+
   render() {
+    let list = this.state.arr.map((val, i) => <h3 key={i}>{val}</h3>)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={e => this.updateFilter(e.target.value)}/>
+        {list}
       </div>
     );
   }
