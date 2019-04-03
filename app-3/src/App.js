fe64d3656717b7a2e8b843e4572b7749538eb218
Aster Brynn <aster.gibby@gmail.com>
2019-04-02 18:59:00 -0600

    3 fini

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -5,23 +5,26 @@ class App extends Component {
   constructor() {
     super();
     this.state={
-      arr: ["spaghoot", "pizza", "croissants", "sausage", "milk"]
+      arr: ["spaghoot", "pizza", "croissants", "sausage", "milk"],
+      userInput: ""
     }
   }
 
-  updateFilter = e => {
-    console.log(e)
+  updateFilter = input => {
     this.setState({
-      userInput: e
+      userInput: input
     })
   }
-
+  
   render() {
-    let list = this.state.arr.map((val, i) => <h3 key={i}>{val}</h3>)
+    let filteredList = this.state.arr.filter((val, i) => {
+      return val.includes(this.state.userInput)
+    }).map((val, i) => <h3 key={i}>{val}</h3>)
+    console.log(filteredList)
     return (
       <div className="App">
         <input onChange={e => this.updateFilter(e.target.value)}/>
-        {list}
+        {filteredList.length > 0 ? filteredList : this.state.arr}
       </div>
     );
   }
